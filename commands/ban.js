f50b2d86bb32da8f375de458b4577fb8fe51c9c9
Al <alexsise.ur@gmail.com>
2022-04-10 22:32:00 +0300

    Исправил описание reason

--- commands/ban.js
+++ commands/ban.js
@@ -5,13 +5,12 @@ module.exports = {
         .setName('ban')
         .setDescription('Ban a specified user')
         .addUserOption(option => option.setName('target').setDescription('The member to ban').setRequired(true))
-        .addStringOption(option => option.setName('reason').setDescription('The member to ban').setRequired(false)),
+        .addStringOption(option => option.setName('reason').setDescription('Reason to ban').setRequired(false)),
 
         async execute(interaction) {
             const target = interaction.options.getUser('target');
             const guild = interaction.guild;
             const reason = interaction.options.getString('reason') ?? "Not specified reason";
-
             guild.members.ban(target, {reason});
             return interaction.reply(`${target.username} was banned due to ${reason}`)
         }
