4b2ec252fe1da7237b5a3846ea65b248bcc365f9
Al <alexsise.ur@gmail.com>
2022-04-13 20:23:05 +0300

    Ивенты были перенесены в отдельные файлы

---
+++ events/interactionCreate.js
@@ -0,0 +1,23 @@
+const cl = require('../index.js');
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        if(!interaction.isCommand()) return;
+
+        const client = cl.client;
+
+        const command  = client.commands.get(interaction.commandName)
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction);
+        } 
+        
+        catch (error) {
+            console.error(error);
+            await interaction.reply({content : 'There was an unexpected error while executing the command', ephemeral:true})
+        }
+    }
+}
---
+++ events/ready.js
@@ -0,0 +1,10 @@
+const cl = require('../index.js');
+
+module.exports = { 
+    name: 'ready',
+    once: true,
+    execute() {
+        const bot = cl.client.user;
+        console.log(`Ready! Logged in as ${bot.tag}`)
+    }
+};
