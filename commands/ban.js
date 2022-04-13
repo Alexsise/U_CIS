d7274fc15c3408fafb7b2004443bb497c6465d03
Al <alexsise.ur@gmail.com>
2022-04-13 12:44:59 +0300

    Теперь команды ban и kick проверяют два условия:
    1) Использующий обладает правами, используемыми в командах ('BAN_MEMBERS' и 'KICK_MEMBERS')
    2) Выбранная цель может быть заблокирована/выгнана

--- commands/ban.js
+++ commands/ban.js
@@ -8,10 +8,25 @@ module.exports = {
         .addStringOption(option => option.setName('reason').setDescription('Reason to ban').setRequired(false)),
 
         async execute(interaction) {
-            const target = interaction.options.getUser('target');
-            const guild = interaction.guild;
-            const reason = interaction.options.getString('reason') ?? "Not specified reason";
-            guild.members.ban(target, {reason});
-            return interaction.reply(`${target.username} was banned due to ${reason}`)
+            try {
+                const guild = interaction.guild;
+                const target = await guild.members.fetch(interaction.options.getUser('target'));
+                const user = await guild.members.fetch(interaction.user);
+                const reason = interaction.options.getString('reason') ?? "not specified reason";
+            
+            
+                if (!user.permissions.has('BAN_MEMBERS', true)) 
+                return interaction.reply('You don\'t have permissions to ban members.');
+
+                if (!target.bannable)
+                    return interaction.reply('The target cannot be banned.');
+            
+                guild.members.ban(target, {reason})
+                return interaction.reply(`${target.username} was banned due to ${reason}.`);
+            }
+            catch (error){
+                console.log(error);
+                return interaction.reply('Ban issue.');
+            }
         }
 }
--- commands/kick.js
+++ commands/kick.js
@@ -9,10 +9,24 @@ module.exports = {
     .addStringOption(option => option.setName('reason').setDescription('Ban reason')),
     
     async execute(interaction) {
-        const target = interaction.options.getUser('target');
-        const guild = interaction.guild;
-        const reason = interaction.options.getString('reason') ?? "Not specified reason";
-        guild.members.kick(target, reason);
-        return interaction.reply(`${target.username} was kicked due to ${reason}`);
+        try {
+            const guild = interaction.guild;
+            const target = await guild.members.fetch(interaction.options.getUser('target'));
+            const user = await guild.members.fetch(interaction.user);
+            const reason = interaction.options.getString('reason') ?? "not specified reason";
+
+            if (!user.permissions.has('KICK_MEMBERS', true)) 
+            return interaction.reply('You don\'t have permissions to kick members.');
+
+            if (!target.kickable)
+            return interaction.reply('The target cannot be kicked.');
+
+            guild.members.kick(target, reason);
+            return interaction.reply(`${target.username} was kicked due to ${reason}`);
+        }
+        catch (error){
+            console.log(error);
+            return interaction.reply('Kick issue.');
+        } 
     }
 }
