09cc0538579b236a4bd10559c35b18b53f1de66c
Al <alexsise.ur@gmail.com>
2022-04-13 23:15:11 +0300

    Исправлены возвращения в блоке try

--- commands/kick.js
+++ commands/kick.js
@@ -16,17 +16,17 @@ module.exports = {
             const reason = interaction.options.getString('reason') ?? "not specified reason";
 
             if (!user.permissions.has('KICK_MEMBERS', true)) 
-            return interaction.reply({content: 'You don\'t have permissions to kick members.', ephemeral: true});
+            return await interaction.reply({content: 'You don\'t have permissions to kick members.', ephemeral: true});
 
             if (!target_guild.kickable)
-            return interaction.reply({content: 'This target cannot be kicked.', ephemeral: true});
+            return await interaction.reply({content: 'This target cannot be kicked.', ephemeral: true});
 
             guild.members.kick(target_guild, reason);
-            return interaction.reply(`${target.username} was kicked due to ${reason}`);
+            return await interaction.reply(`${target.username} was kicked due to ${reason}`);
         }
         catch (error){
             console.log(error);
-            return interaction.reply('Kick issue.');
+            return interaction.reply({content: 'Kick issue.', ephemeral: true});
         } 
     }
 }
