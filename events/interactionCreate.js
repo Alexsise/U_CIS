8a3afa35678356b0c50494960c92921a4cf86d1d
Al <alexsise.ur@gmail.com>
2022-04-14 10:55:47 +0300

    Избавился от модуля с запакованным Client

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,11 +1,7 @@
-const cl = require("../index.js");
-
 module.exports = {
   name: "interactionCreate",
-  async execute(interaction) {
+  async execute(client, interaction) {
     if (!interaction.isCommand()) return;
-
-    const client = cl.client;
 
     const command = client.commands.get(interaction.commandName);
 
