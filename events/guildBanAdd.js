fb446c6d84c7aa020bb10e0ee69eef88092f72a5
Al <alexsise.ur@gmail.com>
2022-04-17 16:21:59 +0300

    Расширение доступной информации

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -3,24 +3,30 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "guildBanAdd",
   async execute(client, ban) {
-    const guildBanLog = await ban.guild.fetchAuditLogs( {
+    const guildBanLog = await ban.guild.fetchAuditLogs({
       limit: 1,
-      type: 'MEMBER_BAN_ADD',
+      type: "MEMBER_BAN_ADD",
     });
     const banLog = guildBanLog.entries.first();
 
-    if (!banLog) return console.log(`${ban.user.tag} was banned> but Audit Logs didn\`t catch that.`)
+    if (!banLog)
+      return console.log(
+        `${ban.user.tag} was banned, but Audit Logs didn\`t catch that.`
+      );
 
     const channel = client.channels.cache.get("963791132196761620");
-    const user = await ban.user;
 
     const embedMessage = new MessageEmbed()
       .setTitle("Log")
       .setDescription("User was banned.")
       .setColor("#000000")
-      .addField("User", user.tag)
-      .addField("Reason", banLog.reason);
+      .addFields(
+        { name: "Target", value: banLog.target.tag, inline: true },
+        { name: "Executor", value: banLog.executor.tag, inline: true },
+        { name: "Reason", value: banLog.reason ?? "Not specified" }
+      );
 
     await channel.send({ embeds: [embedMessage] });
   },
 };
+
