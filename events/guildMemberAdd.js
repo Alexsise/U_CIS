9e6cdc9f36cf5d0fcac11ef87a18604eb0737eeb
Al <alexsise.ur@gmail.com>
2022-05-05 18:53:55 +0300

    ~

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "guildMemberAdd",
+  async execute(client, member) {
+    const date = new Date().toString();
+    const channel = client.channels.cache.get("963791132196761620");
+    who = member.bot ? "Bot" : "User";
+
+    const embedMessage = new MessageEmbed()
+      .setAuthor({
+        name: `${member.user.tag}`,
+        iconURL: `${member.user.avatarURL()}`,
+      })
+      .setTitle("New member arrived.")
+      .setDescription(date)
+      .setColor("#000000")
+      .addFields(
+        { name: `${who} tag`, value: member.user.tag, inline: true },
+        { name: `${who} id`, value: member.id, inline: true },
+        {
+          name: `${who} creation date`,
+          value: member.user.createdAt.toString(),
+        }
+      );
+
+    await channel.send({ embeds: [embedMessage] });
+  },
+};
