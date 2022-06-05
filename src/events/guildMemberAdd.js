27b7c4c5450d7968444bf0d4b660a712f7c9766d
Al <alexsise.ur@gmail.com>
2022-06-05 13:06:42 +0300

    ~~

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,31 +1,31 @@
-const { MessageEmbed } = require("discord.js");
+// const { MessageEmbed } = require("discord.js");
 
-module.exports = {
-  name: "guildMemberAdd",
-  async execute(client, member) {
-    const date = new Date().toString();
-    const logChannel = member.guild.channels.cache.find(
-      (channel) => channel.name === "log"
-    );
-    who = member.bot ? "Bot" : "User";
+// module.exports = {
+//   name: "guildMemberAdd",
+//   async execute(client, member) {
+//     const date = new Date().toString();
+//     const logChannel = member.guild.channels.cache.find(
+//       (channel) => channel.name === "log"
+//     );
+//     who = member.bot ? "Bot" : "User";
 
-    const embedMessage = new MessageEmbed()
-      .setAuthor({
-        name: `${member.user.tag}`,
-        iconURL: `${member.user.avatarURL()}`,
-      })
-      .setTitle("New member arrived.")
-      .setDescription(date)
-      .setColor("#000000")
-      .addFields(
-        { name: `${who} tag`, value: member.user.tag, inline: true },
-        { name: `${who} id`, value: member.id, inline: true },
-        {
-          name: `${who} creation date`,
-          value: member.user.createdAt.toString(),
-        }
-      );
+//     const embedMessage = new MessageEmbed()
+//       .setAuthor({
+//         name: `${member.user.tag}`,
+//         iconURL: `${member.user.avatarURL()}`,
+//       })
+//       .setTitle("New member arrived.")
+//       .setDescription(date)
+//       .setColor("#000000")
+//       .addFields(
+//         { name: `${who} tag`, value: member.user.tag, inline: true },
+//         { name: `${who} id`, value: member.id, inline: true },
+//         {
+//           name: `${who} creation date`,
+//           value: member.user.createdAt.toString(),
+//         }
+//       );
 
-    await logChannel.send({ embeds: [embedMessage] });
-  },
-};
+//     await logChannel.send({ embeds: [embedMessage] });
+//   },
+// };
