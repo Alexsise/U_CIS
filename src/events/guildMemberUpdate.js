79db63115e7d2438def4ba9722e3dd8ac43b3c23
Al <alexsise.ur@gmail.com>
2022-06-12 14:17:45 +0300

    new event: guildMemberUpdate

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -1,34 +1,32 @@
-// const { MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
-// module.exports = {
-//   name: "guildMemberUpdate",
-//   async execute(client, oldMember, newMember) {
-//     const date = new Date().toString();
-//     const logChannel = oldMember.guild.channels.cache.find(
-//       (channel) => channel.name === "log"
-//     );
-//     if (oldMember.displayName !== newMember.displayName) {
-//       const embedMessage = new MessageEmbed()
-//         .setAuthor({
-//           name: `${oldMember.user.tag}`,
-//           iconURL: `${oldMember.user.avatarURL()}`,
-//         })
-//         .setTitle("Guild member`s name was updated.")
-//         .setDescription(date)
-//         .setColor("#000000")
-//         .addFields(
-//           {
-//             name: "Old Name",
-//             value: oldMsg,
-//           },
-//           {
-//             name: "New Name",
-//             value: newMsg,
-//           }
-//         );
-//       await logChannel.send({ embeds: [embedMessage] });
-//     }
-
-
-//   },
-// };
+module.exports = {
+  name: "guildMemberUpdate",
+  async execute(client, oldMember, newMember) {
+    const date = new Date().toString();
+    const logChannel = oldMember.guild.channels.cache.find(
+      (channel) => channel.name === "log"
+    );
+    const embedMessage = new MessageEmbed()
+      .setAuthor({
+        name: `${oldMember.user.tag}`,
+        iconURL: `${oldMember.user.avatarURL()}`,
+      })
+      .setTitle("User was updated")
+      .setDescription(date);
+    if (oldMember.nickname != newMember.nickname)
+      embedMessage.addFields(
+        {
+          name: "Old Nickname",
+          value: oldMember.displayName,
+          inline: true,
+        },
+        {
+          name: "New Nickname",
+          value: newMember.displayName,
+          inline: true,
+        }
+      );
+    logChannel.send({ embeds: [embedMessage] });
+  },
+};
