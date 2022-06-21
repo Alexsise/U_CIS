64d7620595b7b29fd024911e9579dc66b441dbff
Al <alexsise.ur@gmail.com>
2022-06-21 11:12:17 +0300

    Подправлено описание команды

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with the pong!"),
+    .setDescription("Replies with the pong"),
   async execute(interaction) {
     await interaction.reply({content: "Pong!", ephemeral : true});
   },
